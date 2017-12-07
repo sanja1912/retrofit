81fb054b8e259919ace14cc20b02916d7775007d
sanja1912 <sanja.milunovic93@gmail.com>
2017-12-07 13:10:48 +0100

    Retrofit and RxJava

--- app/src/main/java/com/example/dev7/retrofit/activity/MainActivity.java
+++ app/src/main/java/com/example/dev7/retrofit/activity/MainActivity.java
@@ -6,22 +6,17 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
-
 import com.example.dev7.retrofit.R;
 import com.example.dev7.retrofit.adapter.MovieAdapter;
-import com.example.dev7.retrofit.model.Movie;
 import com.example.dev7.retrofit.model.MovieResponse;
 import com.example.dev7.retrofit.rest.MovieApiService;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-import rx.Observable;
-import rx.Observer;
+import rx.Subscriber;
 import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG=MainActivity.class.getSimpleName();
@@ -46,14 +41,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         connectAndGetApiData();
-
-
-
     }
-
-
-
-
 
     private void connectAndGetApiData() {
         if(retrofit==null){
@@ -65,51 +53,25 @@ public class MainActivity extends AppCompatActivity {
         }
         movieApiService=retrofit.create(MovieApiService.class);
 
-        Observable<List<MovieResponse>>observable=movieApiService.getTopRatedMovies(API_KEY);
-        observable.subscribe(new Observer<List<MovieResponse>>() {
-            @Override
-            public void onCompleted() {
-                Log.d(TAG,"In onComplete()");
-            }
+        movieApiService.getTopRatedMovies(API_KEY)
+                .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<MovieResponse>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d(TAG, "In onComplete()");
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+                    @Override
+                    public void onNext(MovieResponse movieResponse) {
+                        Log.d(TAG, "In onNext()");
 
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG,"In onError()");
-            }
-
-            @Override
-            public void onNext(List<MovieResponse> movieResponses) {
-                Log.d(TAG,"In onNext()");
-                for(int i=0;i<movieResponses.size();i++) {
-                    adapter.setMovies(movieResponses.get(i).getResults());
+                        adapter.setMovies(movieResponse.getResults());
+                        adapter.notifyDataSetChanged();
+                    }
                 }
-            }
-        });
-
-
-//        Observable<MovieResponse> observable =movieApiService.getTopRatedMovies(API_KEY);
-//        observable.subscribe(new Observer<MovieResponse>() {
-//            @Override
-//            public void onCompleted() {
-//                Log.d(TAG,"In onComplete()");
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//                Log.d(TAG,"In onError()");
-//            }
-//
-//            @Override
-//            public void onNext(MovieResponse movieResponse) {
-//                Log.d(TAG,"In onNext()");
-////                for(int i=0;i<movieResponses.size();i++) {
-////                    MovieResponse movie=new MovieResponse();
-////                    movie.setResults(movieResponses.get(i).getResults());
-//
-//                    adapter.setMovies(movieResponse.getResults());
-////                }
-//            }
-//        });
+                );
     }
 
     @Override
